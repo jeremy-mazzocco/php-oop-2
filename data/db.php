9b4638ec899d2a22fdc35762bd84e27954ad95e4
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-07-03 15:58:50 +0100

    fixes

--- data/db.php
+++ data/db.php
@@ -3,5 +3,11 @@
 require_once("./models/prodotto.php");
 require_once("./models/animale.php");
 
-$animale = new Animale("cane", "cuccia");
-$prodotto = new Prodotto("cuccia", "img", "Supercuccia", "119.00", "icon");
+$cane = new Animale("cane");
+$gatto = new Animale("gatto");
+
+$cuccia = new Card("cuccia", "img", "Supercuccia", 119, "icon", $cane);
+$osso = new Card("osso", "img", "Superosso", 9, "icon", $cane);
+$crocchette = new Card("crocchette", "img", "Croc Croc", 119, "icon", $cane);
+$rimuoviPeli = new Card("rimuovi peli", "img", "Peli-no", 18, "icon", $gatto);
+$lettiera = new Card("lettiera", "img", "SuperLettiera", 59, "icon", $gatto);
--- models/animale.php
+++ models/animale.php
@@ -2,37 +2,19 @@
 
 class Animale
 {
-
     private $tipo;
-    private $articolo;
 
-    public function __construct($tipo, $articolo)
+    public function __construct($tipo)
     {
-
         $this->setTipo($tipo);
-        $this->setArticolo($articolo);
     }
 
     public function getTipo()
     {
-
         return $this->tipo;
     }
     public function setTipo($tipo)
     {
-
         $this->tipo = $tipo;
-    }
-
-    public function getArticolo()
-    {
-
-        return $this->articolo;
-    }
-
-    public function setArticolo($articolo)
-    {
-
-        $this->articolo = $articolo;
     }
 }
